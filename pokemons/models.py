c45dd0afdf38875e8a05a70cbf81f108710bdf8a
shiishiji <szopinskidamian97@gmail.com>
2017-10-05 02:58:39 +0200

    Added abstract class statistcs, created training methods, added catching system

--- pokemons/models.py
+++ pokemons/models.py
@@ -15,6 +15,13 @@ class Trainer(AbstractUser):
 class Type(models.Model):
     name = models.CharField(max_length=25)
 
+    effective = 2  # super efektywnosc
+    resistant = 0.5  # odpornosc
+    not_effective = 0.5  # mala efektywnosc
+    vulnerability = 2  # podatnosc
+    immune = 0  # nietykalnosc
+    no_effect = 0  # brak efektu
+
     def __str__(self):
         return self.name
 
@@ -143,8 +150,56 @@ class Statistics(models.Model):
 
 
 class Attributes(Statistics):
-    pass
+    def_increases = {
+        'attack': 1,
+        'defense': 1,
+        'special_attack': 1,
+        'special_defense': 1,
+        'agility': 1,
+        'health': 5,
+    }
+
+    def up_all(self, increases=def_increases):
+        counter = 0
+        for x, y in increases:
+            for i in range(y):
+                if counter == 0:
+                    self.up_attack()
+                elif counter == 1:
+                    self.up_defense()
+                elif counter == 2:
+                    self.up_special_attack()
+                elif counter == 3:
+                    self.up_special_defense()
+                elif counter == 4:
+                    self.up_agility()
+                elif counter == 5:
+                    self.up_health()
 
 
 class Trainings(Statistics):
-    pass
+
+    # TRAIN x NUMBER OF TIMES
+    def train_attack(self, times=1):
+        for i in range(times):
+            self.up_attack()
+
+    def train_defense(self, times=1):
+        for i in range(times):
+            self.up_defense()
+
+    def train_special_attack(self, times=1):
+        for i in range(times):
+            self.up_special_attack()
+
+    def train_special_defense(self, times=1):
+        for i in range(times):
+            self.up_special_defense()
+
+    def train_agility(self, times=1):
+        for i in range(times):
+            self.up_agility()
+
+    def train_health(self, times=1):
+        for i in range(times):
+            self.up_health()
